8d2a375ccf66a55ffa7ea2445aa929916f209eea
ADITYA VERMA <adityav1304@gmail.com>
2024-03-22 14:35:35 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -8,7 +8,7 @@ function Navbar() {
     <div>
       <nav>
         <ul className="navbar-menu">
-          <li><a rel="noreferrer" target='_blank' href="https://adityaver.netlify.app/"><FontAwesomeIcon icon={faChrome} /> Protfolio</a></li>
+          <li><a rel="noreferrer" target='_blank' href="https://adityaver.netlify.app/"><FontAwesomeIcon icon={faChrome} /> Portfolio</a></li>
           <li><a rel="noreferrer" target='_blank' href="https://github.com/ADITYAVOFFICIAL/Autocomplete_ReactJS"><FontAwesomeIcon icon={faGithub} /> GitHub</a></li>
           <li><a rel="noreferrer" target='_blank' href="https://www.linkedin.com/in/aditya-verma-real/"><FontAwesomeIcon icon={faLinkedin} /> Linkedin</a></li>
         </ul>
